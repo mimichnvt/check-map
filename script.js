9763c60af731af71975223f0dda2093f58991fe0
Emilie <emilie.chnvt@gmail.com>
2024-01-09 11:43:36 +0100

    finished function mdp

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var myInput = document.getElementById('psw');
 var letter = document.getElementById('letter');
 var capital = document.getElementById('capital');
 var number = document.getElementById('number');
-var lentgh = document.getElementById('lentgh');
+var length = document.getElementById('length');
 
 
 // lorsque l'utilisateur cliquesur le champ du mdp, afficher la boite de message
@@ -30,4 +30,41 @@ myInput.onkeyup = function (){
         letter.classList.remove('valid');
         letter.classList.add('invalid');
     }
+
+    //valider les lettres majuscules
+    var upperCaseLetters = /[A-Z]/g
+    if(myInput.value.match(upperCaseLetters)){
+        //si mdp contient lettre majuscules, enlever class invalid et ajouter class valid
+        capital.classList.remove('invalid');
+        capital.classList.add('valid');
+    }else{
+        //si non, enlever class valid et ajouter class invalid
+        capital.classList.remove('valid');
+        capital.classList.add('invalid');
+    }
+
+
+    //valider les nombres
+    var numbers = /[0-9]/g
+    if(myInput.value.match(numbers)){
+        //si mdp contient chiffre, enlever class invalid et ajouter class valid
+        number.classList.remove('invalid');
+        number.classList.add('valid');
+    }else{
+        //si non, enlever class valid et ajouter class invalid
+        number.classList.remove('valid');
+        number.classList.add('invalid');
+    }
+
+    //valider les 8 caractères minimum
+    if(myInput.value.length >= 8){
+        //si mdp contient 8 caractères minimum, enlever class invalid et ajouter class valid
+        length.classList.remove('invalid');
+        length.classList.add('valid');
+    }else{
+        //si non, enlever class valid et ajouter class invalid
+        length.classList.remove('valid');
+        length.classList.add('invalid');
+    }
 }
+
